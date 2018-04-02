80d0cda0d132f20c29d5b653e61c7929174afca4
test1 <inner_ert@qq.com>
2018-04-02 16:27:17 +0800

    1

--- web/views.py
+++ web/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.shortcuts import render_to_response  #render_to_response是什么
+import json
 # Create your views here.
 
 def Login(request):
-     return HttpResponse('index,哈哈哈')
+     if request.method=='GET':
+          result={}
+          username = request.GET.get('username')
+          mobile = request.GET.get('mobile')
+          data = request.GET.get('data')
+          result['user'] = username
+          result['mobileNum'] = mobile
+          result['data'] = data
+          result = json.dumps(result)
+          return HttpResponse(result,content_type='application/json')
+     else:
+          return render_to_response('login.html')
